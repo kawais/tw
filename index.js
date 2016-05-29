d312e8e8e7e26ca4c70f1fa4c70269774328314b
Wang Yi Ping <kawais@163.com>
2016-05-29 21:40:01 +0800

    add some sample

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ var data = require('./data.js');
 var pos = require('./pos.js');
 var POS = new pos();
 
-console.log('买3免1');
+console.log('买3免1 有优惠');
 POS.printList(data.getOrder(0), data.getPolicy('BUY_THREE_GET_ONE_FREE'));
-
 console.log("\r\n====================\r\n");
-console.log('满百减十');
-POS.printList(data.getOrder(1), data.getPolicy('SINGLE_ITEM_BUY_HUNDRED_DISCOUNT_TEN'));
+console.log('买3免1 无优惠');
+POS.printList(data.getOrder(1), data.getPolicy('BUY_THREE_GET_ONE_FREE'));
+console.log("\r\n====================\r\n");
+
+console.log('满百减十 有优惠');
+POS.printList(data.getOrder(2), data.getPolicy('SINGLE_ITEM_BUY_HUNDRED_DISCOUNT_TEN'));
+console.log("\r\n====================\r\n");
+console.log('满百减十 无优惠');
+POS.printList(data.getOrder(3), data.getPolicy('SINGLE_ITEM_BUY_HUNDRED_DISCOUNT_TEN'));
